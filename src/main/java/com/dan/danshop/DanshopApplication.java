06f2ad4b7ed079c41952175c6db6c84a75369bcb
Dan <danchoi.dev@gmail.com>
2026-04-21 09:40:49 +0900

    init: project setup

---
+++ src/main/java/com/dan/danshop/DanshopApplication.java
@@ -0,0 +1,13 @@
+package com.dan.danshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dan/danshop/DanshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dan.danshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
