d05ffaddd9ce2bd33f10c315182dc58991af0545
Dan <danchoi.dev@gmail.com>
2026-04-28 21:29:13 +0900

    주문취소, 재고복원

--- src/main/java/com/dan/danshop/domain/order/controller/OrderController.java
+++ src/main/java/com/dan/danshop/domain/order/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.dan.danshop.domain.order.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +16,17 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @PostMapping("/order")
+    @PostMapping("/orders")
     public ResponseEntity<?> createOrder(@RequestBody CreateRequest createRequest) {
 
         orderService.createOrder(createRequest);
 
         return ResponseEntity.status(HttpStatus.CREATED).body("주문 생성 완료");
+    }
+
+    @PatchMapping("/orders/{id}/cancel")
+    public ResponseEntity<?> cancelOrder(Long orderId) {
+        orderService.cancelOrder(orderId);
+        return ResponseEntity.status(HttpStatus.OK).body("주문 취소 완료");
     }
 }
