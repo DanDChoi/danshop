825ad8f5df46563291abdacfca1027f5654d8317
Dan <danchoi.dev@gmail.com>
2026-05-03 23:33:55 +0900

    n+1 해결

--- src/main/java/com/dan/danshop/domain/order/repository/OrderRepository.java
+++ src/main/java/com/dan/danshop/domain/order/repository/OrderRepository.java
@@ -4,9 +4,13 @@ import com.dan.danshop.domain.order.entity.Order;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
 //    Page<Order> findByOrderNameContaining(String keyword, Pageable pageable);
 
+    //N+1 해결
+    @Query("SELECT o FROM Order o JOIN FETCH o.user")
+    Page<Order> findAllWithUser(Pageable pageable);
 }
