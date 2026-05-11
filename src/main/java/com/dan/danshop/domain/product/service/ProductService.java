26d1ec9cfd18c41c9b1a5366890a3b86a55d82e7
Dan <danchoi.dev@gmail.com>
2026-05-11 20:25:29 +0900

    redis 적용

--- src/main/java/com/dan/danshop/domain/product/service/ProductService.java
+++ src/main/java/com/dan/danshop/domain/product/service/ProductService.java
@@ -7,6 +7,8 @@ import com.dan.danshop.domain.product.entity.Product;
 import com.dan.danshop.domain.product.repository.ProductRepository;
 import com.dan.danshop.global.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -19,6 +21,7 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    @CacheEvict(value = "products", allEntries = true)
     public void addProduct(AddRequest addRequest) {
 
         Product addProduct = Product.from(addRequest);
@@ -26,6 +29,7 @@ public class ProductService {
         productRepository.save(addProduct);
     }
 
+    @CacheEvict(value = "products", allEntries = true)
     public void updateProduct(Long productNo, UpdateRequest updateRequest) {
         Product product = productRepository.findById(productNo)
                 .orElseThrow(() -> new BusinessException(PRODUCT_NOT_FOUND));
@@ -40,6 +44,7 @@ public class ProductService {
         return ProductResponse.from(product);
     }
 
+    @Cacheable("products")
     public Page<ProductResponse> findProductList(int page, int size, String keyword) {
         PageRequest pageRequest = PageRequest.of(page, size);
 
@@ -53,6 +58,7 @@ public class ProductService {
 
     }
 
+    @CacheEvict(value = "products", allEntries = true)
     public void deleteProduct(Long productNo) {
         productRepository.deleteById(productNo);
     }
