9c6bafd46ca9ef24a75dcab9af4ca5a210b0b45e
Dan <danchoi.dev@gmail.com>
2026-05-28 23:05:03 +0900

    coupon 작업

--- src/main/java/com/dan/danshop/domain/coupon/repository/CouponRepository.java
+++ src/main/java/com/dan/danshop/domain/coupon/repository/CouponRepository.java
@@ -1,7 +1,17 @@
 package com.dan.danshop.domain.coupon.repository;
 
 import com.dan.danshop.domain.coupon.entity.Coupon;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p From Coupon p WHERE p.id = :id")
+    Optional<Coupon> findByIdWithLock(@Param("id") Long id);
 }
