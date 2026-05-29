a657d2c146a6f313c9f08f1039af451d0b648d01
Dan <danchoi.dev@gmail.com>
2026-05-29 23:18:20 +0900

    coupon service complete

--- src/main/java/com/dan/danshop/global/exception/ErrorCode.java
+++ src/main/java/com/dan/danshop/global/exception/ErrorCode.java
@@ -10,7 +10,10 @@ public enum ErrorCode {
     PRODUCT_NOT_FOUND("상품을 찾을 수 없습니다."),
     ORDER_NOT_FOUND("주문을 찾을 수 없습니다."),
     IMPOSSIBLE_CANCEL_ORDER("취소 가능한 주문이 아닙니다."),
-    NOT_ORDERED_USER("본인의 주문만 취소 가능합니다.");
+    NOT_ORDERED_USER("본인의 주문만 취소 가능합니다."),
+    COUPON_NOT_FOUND("쿠폰을 찾을 수 없습니다."),
+    COUPON_SOLD_OUT("선착순 마감된 쿠폰입니다."),
+    COUPON_ALREADY_ISSUED("이미 발급받은 쿠폰입니다.");
 
     private final String message;
 
